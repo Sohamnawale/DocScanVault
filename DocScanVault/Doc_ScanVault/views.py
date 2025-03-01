e8d64f0942d099420c517cefc64c9dfd753aad42
Sohamnawale <149603509+Sohamnawale@users.noreply.github.com>
2025-03-01 14:01:51 +0000

    feat: Introduction Page Added

--- DocScanVault/Doc_ScanVault/views.py
+++ DocScanVault/Doc_ScanVault/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
+import os
+from django.conf import settings
+from django.http import JsonResponse
+from PIL import Image
+from IPython.display import Markdown
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def index(request):
     if request.method == "GET":
-        return render(request, 'DocScanVault/index.html')
+        return render(request, 'Doc_ScanVault/index.html')
